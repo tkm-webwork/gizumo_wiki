76646164310cab0335dce828bd18638c012dbf21
n-komatsu <komatsu@gizumo-inc.jp>
2019-05-13 15:10:55 +0900

    run dev:storybookに静的アセットディレクテリを指定するオプションを追加、TextAreaコンポーネントのストーリーを修正

--- src/js/components/atoms/Textarea/index.stories.js
+++ src/js/components/atoms/Textarea/index.stories.js
@@ -11,17 +11,58 @@ storiesOf('ATOMS|Textarea', module)
                   placeholder="記事の本文をマークダウン記法で入力してください。"
                   required
                   rows="8"
-                  white-bg
-                  :value="content"
+                  :value="value"
                   @updateValue="updateValue"
                 >
                 </app-textarea>`,
-      data() {
-        return {
-          title: '',
-          content: '',
+      props: {
+        name: {
+          type: String,
+          default: 'inputName',
+        },
+        value: {
+          type: String,
+          default: text('value', ''),
+        },
+        placeholder: {
+          type: String,
+          default: '入力してください'
+        },
+        required: {
+          type: Boolean,
+          default: true,
+        },
+      },
+      computed: {
+        classes() {
+          return {
+            text: true,
+          }
         }
       },
+      methods: {
+        updateValue($event) {
+          this[$event.target.name] = $event.target.value;
+        }
+      }
+    }
+  }, {notes: note});
+
+
+storiesOf('ATOMS|Textarea', module)
+  .addDecorator(withKnobs)
+  .add(' whiteBg', () => {
+    return {
+      template: `<app-textarea
+                  name="content"
+                  placeholder="記事の本文をマークダウン記法で入力してください。"
+                  required
+                  rows="8"
+                  white-bg
+                  :value="value"
+                  @updateValue="updateValue"
+                >
+                </app-textarea>`,
       props: {
         name: {
           type: String,
