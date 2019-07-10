7d5ecbc387986736e61def77488a527ee25cfe9a
tkcco <asd33tyu@gmail.com>
2019-07-10 11:30:35 +0900

    signoutとnotfoundのページのコンポーネントを共通化 #32

--- src/js/components/molecules/index.js
+++ src/js/components/molecules/index.js
@@ -14,6 +14,7 @@ import ArticleEdit from './ArticleEdit';
 import ArticlePost from './ArticlePost';
 import ArticleDetail from './ArticleDetail';
 import DeleteModal from './Modal';
+import Notice from './Notice';
 
 export {
   SigninForm,
@@ -32,4 +33,5 @@ export {
   ArticlePost,
   ArticleDetail,
   DeleteModal,
+  Notice,
 };
