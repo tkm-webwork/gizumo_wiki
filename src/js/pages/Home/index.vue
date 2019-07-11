519d09eedcd50f1ab6d6b8e20b5f766aa0bd6277
tkcco <asd33tyu@gmail.com>
2019-07-11 20:25:18 +0900

    home画面の作成 途中 #25

--- src/js/pages/Home/index.vue
+++ src/js/pages/Home/index.vue
@@ -1,9 +1,54 @@
 <template lang="html">
-  <p>Home</p>
+  <div>
+    <div>
+      <app-heading>関連記事の多いカテゴリー</app-heading>
+
+      <ul>
+        <li v-for="category in categoryList" :key="category.id">
+          {{ category.name }}
+        </li>
+      </ul>
+
+      <app-router-link to="/categories">
+        カテゴリー一覧へ
+      </app-router-link>
+    </div>
+    <div>
+      <app-heading>最新記事</app-heading>
+
+      <ul>
+        <li v-for="article in articleList" :key="article.id">
+          {{ article.title }}
+        </li>
+      </ul>
+
+      <app-router-link to="/articles">
+        記事一覧へ
+      </app-router-link>
+    </div>
+  </div>
 </template>
 
 <script>
+import { Heading, RouterLink } from '@Components/atoms';
+
 export default {
+  components: {
+    appHeading: Heading,
+    appRouterLink: RouterLink,
+  },
+  computed: {
+    articleList() {
+      return this.$store.state.articles.articleList;
+    },
+    categoryList() {
+      return this.$store.state.categories.categoryList;
+    },
+  },
+  created() {
+    this.$store.dispatch('getAllCategories');
+    this.$store.dispatch('articles/getAllArticles');
+  },
 };
 </script>
 
