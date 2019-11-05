f63fb572a39467890635c9b30a3a292b8639b5b6
井上拓 <>
2019-11-05 10:17:42 +0900

    課題２

--- src/js/pages/Categories/Edit.vue
+++ src/js/pages/Categories/Edit.vue
@@ -3,7 +3,12 @@
     <app-category-edit
       :disabled="loading ? true : false"
       :access="access"
+      :error-message="errorMessage"
+      :done-message="doneMessage"
+      :update-category-name="updateCategoryName"
       @clearMessage="clearMessage"
+      @updateCategory="updateCategory"
+      @updateValue="editedCategory"
     />
   </div>
 </template>
@@ -22,10 +27,31 @@ export default {
     loading() {
       return this.$store.state.categories.loading;
     },
+    errorMessage() {
+      return this.$store.state.categories.errorMessage;
+    },
+    doneMessage() {
+      return this.$store.state.categories.doneMessage;
+    },
+    updateCategoryName() {
+      return this.$store.state.categories.updateCategoryName;
+    },
+  },
+  created() {
+    const categoryId = this.$route.params.id;
+    this.$store.dispatch('categories/getCategory', parseInt(categoryId, 10));
   },
   methods: {
     clearMessage() {
       this.$store.dispatch('categories/clearMessage');
+    },
+    editedCategory($event) {
+      this.$store.dispatch('categories/editedCategory', $event.target.value);
+    },
+    updateCategory() {
+      const categoryId = this.$route.params.id;
+      const categoryName = this.updateCategoryName;
+      this.$store.dispatch('categories/updateCategory', { categoryId, categoryName });
     },
   },
 };
