55f55278641de4d672ec4882d0e884010dc36d5d
kan-kan <kancan877@gmail.com>
2019-12-03 10:30:06 +0900

    課題４の作成

---
+++ src/js/components/molecules/ArticlePost/index.vue
@@ -0,0 +1,177 @@
+<template lang="html">
+  <div class="article-post">
+    <div v-if="doneMessage" class="article-post__notice--update">
+      <app-text bg-success>{{ doneMessage }}</app-text>
+    </div>
+    <div class="article-post__columns">
+      <section class="article-post-editor">
+        <app-heading :level="1">記事の新規作成</app-heading>
+        <app-heading :level="2" class="article-post-editor-title">カテゴリーの選択</app-heading>
+        <app-select
+          v-validate="'required'"
+          name="category"
+          data-vv-as="カテゴリー"
+          :value="currentCategoryName"
+          :error-messages="errors.collect('category')"
+          @updateValue="$emit('selectedArticleCategory', $event)"
+        >
+          <option
+            value=""
+            disabled
+            selected
+          >---</option>
+          <option
+            v-for="category in categoryList"
+            :key="category.id"
+            :value="category.name"
+          >
+            {{ category.name }}
+          </option>
+        </app-select>
+        <app-heading :level="2" class="article-post-editor-title">タイトル本文</app-heading>
+        <div class="article-post-form">
+          <app-input
+            v-validate="'required'"
+            name="title"
+            type="text"
+            placeholder="記事のタイトルを入力してください"
+            white-bg
+            data-vv-as="記事のタイトル"
+            :error-messages="errors.collect('title')"
+            :value="articleTitle"
+            @updateValue="$emit('editedTitle', $event)"
+          />
+        </div>
+        <div class="article-post-form">
+          <app-textarea
+            v-validate="'required'"
+            name="content"
+            placeholder="記事の本文をマークダウン記法で入力してください。"
+            white-bg
+            data-vv-as="記事の本文"
+            :error-messages="errors.collect('content')"
+            :value="articleContent"
+            @updateValue="$emit('editedContent', $event)"
+          />
+        </div>
+        <app-button
+          class="article-post-submit"
+          button-type="submit"
+          round
+          :disabled="disabled"
+          @click="handleSubmit"
+        >
+          {{ buttonText }}
+        </app-button>
+      </section>
+      <article class="article-post-preview">
+        <app-markdown-preview
+          :markdown-content="markdownContent"
+        ></app-markdown-preview>
+      </article>
+    </div>
+  </div>
+</template>
+
+<script>
+import {
+  Heading, Input, Textarea, MarkdownPreview, Button, Select, Text,
+} from '@Components/atoms';
+
+export default {
+  components: {
+    appHeading: Heading,
+    appInput: Input,
+    appTextarea: Textarea,
+    appMarkdownPreview: MarkdownPreview,
+    appButton: Button,
+    appSelect: Select,
+    appText: Text,
+  },
+  props: {
+    access: {
+      type: Object,
+      default: () => {},
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    currentCategoryName: {
+      type: String,
+      default: '',
+    },
+    categoryList: {
+      type: Array,
+      default: () => [],
+    },
+    errorMessage: {
+      type: String,
+      default: '',
+    },
+    doneMessage: {
+      type: String,
+      default: '',
+    },
+    markdownContent: {
+      type: String,
+      default: '',
+    },
+    articleTitle: {
+      type: String,
+      default: '',
+    },
+    articleContent: {
+      type: String,
+      default: '',
+    },
+  },
+  methods: {
+    handleSubmit() {
+      if (!this.access.create) return;
+      this.$validator.validate().then((valid) => {
+        if (valid) this.$emit('handleSubmit');
+      });
+    },
+  },
+  computed: {
+    buttonText() {
+      if (!this.access.create) return '作成権限がありません';
+      return this.loading ? '作成中...' : '作成';
+    },
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+.article-post {
+
+  &__columns {
+    display: flex;
+    height: 100%;
+  }
+  &-editor {
+    padding-right: 2%;
+    width: 50%;
+    border-right: 1px solid #ccc;
+    &-title {
+      margin-top: 16px;
+    }
+  }
+  &-preview {
+    margin-left: 2%;
+    width: 48%;
+    overflow-y: scroll;
+    background-color: #fff;
+  }
+  &-form {
+    margin-top: 20px;
+  }
+  &-submit {
+    margin-top: 16px;
+  }
+  &__notice--update {
+    margin-bottom: 16px;
+  }
+}
+</style>
