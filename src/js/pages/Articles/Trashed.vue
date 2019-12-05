136044bbde8fc8c2c81d3cefb15c5091466fdc60
shuhei wada <shuu1222@gmail.com>
2019-12-05 18:21:16 +0900

    削除機能コンポーネント追加

---
+++ src/js/pages/Articles/Trashed.vue
@@ -0,0 +1,55 @@
+<template lang="html">
+  <div class="articles">
+    <app-article-trashed
+      :title="title"
+      :target-array="articleTrashed"
+      :access="access"
+      border-gray
+    />
+  </div>
+</template>
+
+<script>
+import { ArticleTrashed } from '@Components/molecules';
+import Mixins from '@Helpers/mixins';
+
+export default {
+  components: {
+    appArticleTrashed: ArticleTrashed,
+  },
+  mixins: [Mixins],
+  beforeRouteUpdate(to, from, next) {
+    const categoryName = to.query.category ? to.query.category : null;
+    this.fetchArticles(categoryName);
+    next();
+  },
+  data() {
+    return {
+      title: '削除済みの記事',
+    };
+  },
+  computed: {
+    articleTrashed() {
+      return this.$store.state.articles.articleList;
+    },
+    access() {
+      return this.$store.getters['auth/access'];
+    },
+  },
+  created() {
+    const categoryName = this.$route.query.category ? this.$route.query.category : null;
+    this.fetchArticles(categoryName);
+  },
+  methods: {
+    fetchArticles(categoryName) {
+      this.$store.dispatch('articles/getTrashedArticles', categoryName)
+        .then(() => {
+          if (this.$store.state.articles.articleList.length === 0) {
+            this.$router.push({ path: '/notfound' });
+          }
+        }).catch(() => {
+        });
+    },
+  },
+};
+</script>
