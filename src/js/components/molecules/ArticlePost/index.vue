fee1035baa804c7db940900757f293312173bcfb
Anna Takahashi <annatakahashi813@gmail.com>
2020-01-22 10:45:36 +0900

    間違って消してしまったファイルを戻しました。

---
+++ src/js/components/molecules/ArticlePost/index.vue
@@ -0,0 +1,36 @@
+<template lang="html">
+  <div class="article-post">
+  </div>
+  <div class="article-post__contents">
+    <section class="article">
+      <app-heading :level="1">記事の新規作成</app-heading>
+      <app-heading
+        :level="2">カテゴリーの選択</app-heading>
+      <app-select
+        v-validate="'required'"
+        name="category"
+        data-vv-as="カテゴリー"
+        :value="categoryName"
+        @updateValue="$emit('selectedArticleCategory', $event)"
+      >
+        <option
+          value=""
+          disabled
+          selected
+        >
+          ---
+        </option>
+        <option
+          v-for="(category) in categoryList"
+          :key="category.id"
+          :value="category.name"
+        >
+          {{ category.name }}
+        </option>
+      <app-select>
+      <app-heading
+        class="article-post-editor-title"
+        :level="2">
+        タイトル・本文
+      </app-heading>
+    </template>
