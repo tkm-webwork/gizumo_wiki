78e8165cb34d8ea30151883664ff8012269461fc
Anna Takahashi <annatakahashi813@gmail.com>
2020-01-22 16:18:02 +0900

    ArticlePostコンポーネントを読み込み、Emitをv-onイベントで受け取る記述、Computed内でリアクティブデータが使用を許可する実装を追加。

--- src/js/pages/Articles/Post.vue
+++ src/js/pages/Articles/Post.vue
@@ -1,5 +1,43 @@
 <template lang="html">
-  <div>
-    ドキュメントの新規作成画面です
-  </div>
+  <app-article-post
+    :article-title="articleTitle"
+    :article-content="articleContent"
+    :category-list="categoryList"
+    @selectedArticleCategory="selectedArticleCategory"
+    @editedTitle="editedTitle"
+    @editContent="editContent"
+    @handleSubmit="handleSubmit"
+  >  
+  </app-article-post>
 </template>
+
+<script>
+import { ArticlePost } from '@Components/molecules';
+
+export default {
+  components: {
+    appArticlePost: ArticlePost,
+  },
+  computed: {
+    articleTitle(){
+      const { title } = this.$store.state.articles.targetArticle;
+      return title;
+    },
+    articleContent(){
+      const { content } = this.$store.state.articles.targetArticle;
+      return content;
+    },
+    categoryList(){
+      const { categoryList } = this.$store.state.categories;
+      return categoryList;
+    },
+  },
+  created(){
+
+  },
+  methods: {
+
+  }
+}
+
+</script>
