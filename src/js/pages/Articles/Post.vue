e134c05db4082b888fb6933595653adb3e0cd19f
Anna Takahashi <annatakahashi813@gmail.com>
2020-01-30 14:02:19 +0900

    新しい記事の作成機能を追加。

--- src/js/pages/Articles/Post.vue
+++ src/js/pages/Articles/Post.vue
@@ -3,12 +3,15 @@
     :article-title="articleTitle"
     :article-content="articleContent"
     :category-list="categoryList"
+    :category-name="categoryName"
+    :access="access"
+    :loading="loading"
+    :markdown-content="markdownContent"
     @selectedArticleCategory="selectedArticleCategory"
     @editedTitle="editedTitle"
-    @editContent="editContent"
+    @editedContent="editedContent"
     @handleSubmit="handleSubmit"
-  >  
-  </app-article-post>
+  />
 </template>
 
 <script>
@@ -18,26 +21,69 @@ export default {
   components: {
     appArticlePost: ArticlePost,
   },
+  data() {
+    return {
+      title: '',
+      content: '',
+    };
+  },
   computed: {
-    articleTitle(){
+    articleTitle() {
       const { title } = this.$store.state.articles.targetArticle;
       return title;
     },
-    articleContent(){
+    articleContent() {
       const { content } = this.$store.state.articles.targetArticle;
       return content;
     },
-    categoryList(){
+    categoryList() {
       const { categoryList } = this.$store.state.categories;
       return categoryList;
     },
+    access() {
+      return this.$store.getters['auth/access'];
+    },
+    categoryName() {
+      return this.$store.state.articles.targetArticle.category.name;
+    },
+    loading() {
+      return this.$store.state.articles.loading;
+    },
+    errorMessage() {
+      return this.$store.state.articles.errrorMessage;
+    },
+    markdownContent() {
+      return `# ${this.articleTitle}\n${this.articleContent}`;
+    },
   },
-  created(){
-
+  created() {
+    this.$store.dispatch('categories/getAllCategories');
+    this.$store.dispatch('articles/initPostArticle');
   },
   methods: {
-
-  }
-}
-
+    selectedArticleCategory($event) {
+      const categoryName = $event.target.value;
+      this.$store.dispatch('articles/selectedArticleCategory', categoryName);
+    },
+    editedTitle($event) {
+      console.log($event);
+      this.$store.dispatch('articles/editedTitle', $event.target.value);
+    },
+    editedContent($event) {
+      console.log($event);
+      this.$store.dispatch('articles/editedContent', $event.target.value);
+    },
+    handleSubmit() {
+      if (this.loading) return;
+      this.$store.dispatch('articles/postArticle').then(() => {
+        this.$router.push({
+          path: '/articles',
+          query: { redirect: '/article/post' },
+        });
+      });
+    },
+  },
+};
 </script>
+<style lang="postcss" scoped>
+</style>
