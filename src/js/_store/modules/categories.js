c755a6653f4c856e47d6ff4f119f5a70a1d1718b
maeta187 <nexus18789@gmail.com>
2020-01-31 11:55:17 +0900

    カテゴリー更新機能の実装のコーディング終了

--- src/js/_store/modules/categories.js
+++ src/js/_store/modules/categories.js
@@ -20,7 +20,7 @@ export default {
       commit('clearMessage');
     },
     getAllCategories({ commit, rootGetters }) {
-      // ユーザーのトークを取得
+      // ユーザーのトークンを取得
       // メソッドとエンドポイントを指定
       axios(rootGetters['auth/token'])({
         method: 'GET',
@@ -65,8 +65,52 @@ export default {
         });
       });
     },
+    getCategory({ commit, rootGetters }, categoryId) {
+      return new Promise((resolve, reject) => {
+        // カテゴリーIDを元にして変更対象のカテゴリーを検索
+        axios(rootGetters['auth/token'])({
+          method: 'GET',
+          url: `/category/${categoryId}`,
+        }).then((response) => {
+          // thenの引数からidとnameのオブジェクトを変数に代入
+          const payload = response.data.category;
+          // updateDetailsを実行し、payloadを引き渡す
+          commit('updateDetail', payload);
+          resolve();
+        }).catch((err) => {
+          commit('failFetchCategory', { message: err.message });
+          reject();
+        });
+      });
+    },
+    updateCategoryName({ commit }, updateValue) {
+      commit('updateCategoryName', updateValue);
+    },
     confirmDeleteCategory({ commit }, { categoryId, categoryName }) {
       commit('confirmDeleteCategory', { categoryId, categoryName });
+    },
+    editCategory({ commit, rootGetters, state }) {
+      commit('toggleLoading');
+      const editId = state.updateCategoryId;
+      const editName = state.updateCategoryName;
+      const data = new URLSearchParams();
+      data.append('id', `${editId}`);
+      data.append('name', `${editName}`);
+      return new Promise((resolve, reject) => {
+        axios(rootGetters['auth/token'])({
+          method: 'PUT',
+          url: `/category/${editId}`,
+          data,
+        }).then(() => {
+          commit('donePutCategory');
+          commit('toggleLoading');
+          resolve();
+        }).catch((err) => {
+          commit('failFetchCategory', { message: err.message });
+          commit('toggleLoading');
+          reject();
+        });
+      });
     },
     deleteCategory({ commit, rootGetters }, categoryId) {
       return new Promise((resolve) => {
@@ -76,7 +120,6 @@ export default {
         }).then((response) => {
           // NOTE: エラー時はresponse.data.codeが0で返ってくる。
           if (response.data.code === 0) throw new Error(response.data.message);
-
           commit('doneDeleteCategory');
           resolve();
         }).catch((err) => {
@@ -98,6 +141,18 @@ export default {
     },
     toggleLoading(state) {
       state.loading = !state.loading;
+    },
+    updateDetail(state, payload) {
+      state.updateCategoryId = payload.id;
+      state.updateCategoryName = payload.name;
+    },
+    updateCategoryName(state, payload) {
+      state.updateCategoryName = payload;
+    },
+    donePutCategory(state) {
+      state.deleteCategoryId = null;
+      state.deleteCategoryName = '';
+      state.doneMessage = 'カテゴリーの更新が完了しました。';
     },
     confirmDeleteCategory(state, { categoryId, categoryName }) {
       state.deleteCategoryId = categoryId;
