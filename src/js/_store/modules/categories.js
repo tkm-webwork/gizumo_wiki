f8a8c271d9b79b1a4ca57cb16d62515cb4bff08e
tomokamura <gangcunzx@gmail.com>
2020-04-09 21:02:55 +0900

    誤って消してしまったファイルの修正

---
+++ src/js/_store/modules/categories.js
@@ -0,0 +1,102 @@
+import axios from '@Helpers/axiosDefault';
+
+export default {
+  namespaced: true,
+  state: {
+    loading: false,
+    errorMessage: '',
+    doneMessage: '',
+    categoryList: [],
+    deleteCategoryId: null,
+    deleteCategoryName: '',
+    updateCategoryId: null,
+    updateCategoryName: '',
+  },
+  getters: {
+    categoryList: state => state.categoryList,
+  },
+  actions: {
+    clearMessage({ commit }) {
+      commit('clearMessage');
+    },
+    getAllCategories({ commit, rootGetters }) {
+      axios(rootGetters['auth/token'])({
+        method: 'GET',
+        url: '/category',
+      }).then((response) => {
+        const payload = { categories: [] };
+        response.data.categories.forEach((val) => {
+          payload.categories.push(val);
+        });
+        commit('doneGetAllCategories', payload);
+      }).catch((err) => {
+        commit('failFetchCategory', { message: err.message });
+      });
+    },
+    postCateogry({ commit, rootGetters }, categoryName) {
+      commit('toggleLoading');
+      const data = new URLSearchParams();
+      data.append('name', categoryName);
+      return new Promise((resolve, reject) => {
+        axios(rootGetters['auth/token'])({
+          method: 'POST',
+          url: '/category',
+          data,
+        }).then(() => {
+          commit('toggleLoading');
+          commit('donePostCategory');
+          resolve();
+        }).catch((err) => {
+          commit('toggleLoading');
+          commit('failFetchCategory', { message: err.message });
+          reject();
+        });
+      });
+    },
+    confirmDeleteCategory({ commit }, { categoryId, categoryName }) {
+      commit('confirmDeleteCategory', { categoryId, categoryName });
+    },
+    deleteCategory({ commit, rootGetters }, categoryId) {
+      return new Promise((resolve) => {
+        axios(rootGetters['auth/token'])({
+          method: 'DELETE',
+          url: `/category/${categoryId}`,
+        }).then((response) => {
+          // NOTE: エラー時はresponse.data.codeが0で返ってくる。
+          if (response.data.code === 0) throw new Error(response.data.message);
+          commit('doneDeleteCategory');
+          resolve();
+        }).catch((err) => {
+          commit('failFetchCategory', { message: err.message });
+        });
+      });
+    },
+  },
+  mutations: {
+    clearMessage(state) {
+      state.errorMessage = '';
+      state.doneMessage = '';
+    },
+    doneGetAllCategories(state, { categories }) {
+      state.categoryList = [...categories];
+    },
+    failFetchCategory(state, { message }) {
+      state.errorMessage = message;
+    },
+    toggleLoading(state) {
+      state.loading = !state.loading;
+    },
+    donePostCategory(state) {
+      state.doneMessage = 'カテゴリーの追加が完了しました。';
+    },
+    confirmDeleteCategory(state, { categoryId, categoryName }) {
+      state.deleteCategoryId = categoryId;
+      state.deleteCategoryName = categoryName;
+    },
+    doneDeleteCategory(state) {
+      state.deleteCategoryId = null;
+      state.deleteCategoryName = '';
+      state.doneMessage = 'カテゴリーの削除が完了しました。';
+    },
+  },
+};
---
+++ src/js/components/molecules/CategoryEdit/index.vue
@@ -0,0 +1,96 @@
+<template lang="html">
+  <form @submit.prevent="handleSubmit">
+    <app-heading :level="1">カテゴリー管理</app-heading>
+    <app-router-link
+      class="category-management-edit__link"
+      block
+      underline
+      key-color
+      hover-opacity
+      to="/categories"
+    >
+      カテゴリー一覧へ戻る
+    </app-router-link>
+    <app-input
+      class="category-management-edit__input"
+      name="updateCategory"
+      type="text"
+      placeholder="カテゴリー名を入力してください"
+      data-vv-as=""
+    />
+    <app-button
+      class="category-management-edit__submit"
+      button-type="submit"
+      round
+      :disabled="disabled || !access.edit"
+    >
+      {{ buttonText }}
+    </app-button>
+
+    <div class="category-management-edit__notice">
+      <app-text bg-error>ここにエラー時のメッセージが入ります</app-text>
+    </div>
+
+    <div class="category-management-edit__notice">
+      <app-text bg-success>ここに更新成功時のメッセージが入ります</app-text>
+    </div>
+  </form>
+</template>
+<script>
+import {
+  Heading, Input, Button, RouterLink, Text,
+} from '@Components/atoms';
+export default {
+  components: {
+    appHeading: Heading,
+    appInput: Input,
+    appButton: Button,
+    appRouterLink: RouterLink,
+    appText: Text,
+  },
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    access: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  computed: {
+    buttonText() {
+      if (!this.access.edit) return '変更権限がありません';
+      return this.disabled ? '更新中...' : '更新';
+    },
+  },
+  methods: {
+    handleSubmit() {
+      if (!this.access.edit) return;
+      this.$emit('clearMessage');
+      this.$validator.validate().then((valid) => {
+        if (valid) this.$emit('エミットするイベント名が入ります');
+      });
+    },
+  },
+};
+</script>
+<style lang="postcss" scoped>
+.category-management-edit {
+  &__title {
+    margin-top: 16px;
+  }
+  &__input {
+    margin-top: 16px;
+  }
+  &__submit {
+    margin-top: 16px;
+  }
+  &__link {
+    margin-top: 16px;
+  }
+  &__notice {
+    margin-top: 16px;
+  }
+}
+</style>
