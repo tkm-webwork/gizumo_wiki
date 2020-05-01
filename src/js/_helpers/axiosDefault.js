e7e4799eab98bdc20444688790f4c3729483d255
YuiseiMaruyama <yuisei.rock@gmail.com>
2020-05-01 16:29:28 +0900

    GizumoWikiの課題2のファイルを変更しました

--- src/js/_helpers/axiosDefault.js
+++ src/js/_helpers/axiosDefault.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default (token = null) => {
   const headers = {};
-  if (token) Object.assign(headers, { Authorization: `Bearer ${token}` });
+  if (token) Object.assign(headers, { Authorization: `Bearer ${token}` }); // 認証をする
 
   return axios.create({
     baseURL: API_BASE_URL,
