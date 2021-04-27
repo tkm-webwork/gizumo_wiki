0d9a88b5d8d207292210e39a1784c568c6d39424
fuchiwaki_rie <0896.riri@gmail.com>
2021-04-27 14:26:07 +0900

    カテゴリー更新機能の実装

--- src/js/pages/Categories/Edit.vue
+++ src/js/pages/Categories/Edit.vue
@@ -3,7 +3,12 @@
     <app-category-edit
       :disabled="loading ? true : false"
       :access="access"
+      :update-category-name="updateCategoryName"
+      :done-message="doneMessage"
+      :error-message="errorMessage"
       @clearMessage="clearMessage"
+      @handleSubmit="updateHandleSubmit"
+      @editedName="editedName"
     />
   </div>
 </template>
@@ -15,7 +20,22 @@ export default {
   components: {
     appCategoryEdit: CategoryEdit,
   },
+  data() {
+    return {
+      id: '',
+      name: '',
+    };
+  },
   computed: {
+    updateCategoryName() {
+      return this.$store.state.categories.updateCategoryName;
+    },
+    errorMessage() {
+      return this.$store.state.categories.errorMessage;
+    },
+    doneMessage() {
+      return this.$store.state.categories.doneMessage;
+    },
     access() {
       return this.$store.getters['auth/access'];
     },
@@ -23,9 +43,22 @@ export default {
       return this.$store.state.categories.loading;
     },
   },
+  created() {
+    const { id } = this.$route.params;
+    this.$store.dispatch('categories/getCategoryDetail', { id });
+    this.$store.dispatch('categories/clearMessage');
+  },
   methods: {
     clearMessage() {
       this.$store.dispatch('categories/clearMessage');
+    },
+    editedName($event) {
+      this.$store.dispatch('categories/editedName', $event.target.value);
+    },
+    updateHandleSubmit() {
+      const { id } = this.$route.params;
+      if (this.loading) return;
+      this.$store.dispatch('categories/updateCategory', { id });
     },
   },
 };
