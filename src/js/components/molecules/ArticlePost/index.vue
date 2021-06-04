5f93febc5d5a2a83eafa8c3487887d0e201d1850
mihonakagawa <ohim212welldone@gmail.com>
2021-06-04 17:06:19 +0900

    [add] template内にwrapperのような役目としてdivタグを追加

--- src/js/components/molecules/ArticlePost/index.vue
+++ src/js/components/molecules/ArticlePost/index.vue
@@ -1,76 +1,85 @@
 <template lang="html">
-  <div class="article-post">
-    <section class="article-post-editor">
-      <app-heading :level="1">
-        記事の新規作成
-      </app-heading>
-      <app-heading :level="2">
-        カテゴリーの選択
-      </app-heading>
-      <app-select
-        v-validate="'required'"
-        name="category"
-        data-vv-as="カテゴリー"
-        :value="currentCategoryName"
-        :error-messages="errors.collect('category')"
-        @updateValue="$emit('selectedArticleCategory', $event)"
-      >
-        <option
-          value=""
-          selected
-          disabled
+  <div class="article-edit">
+    <div v-if="errorMessage" class="article-post__notice--error">
+      <app-text bg-danger>{{ errorMessage }}</app-text>
+    </div>
+    <div class="article-post__columns">
+      <section class="article-post-editor">
+        <app-heading :level="1">
+          記事の新規作成
+        </app-heading>
+        <app-heading :level="2">
+          カテゴリーの選択
+        </app-heading>
+        <app-select
+          v-validate="'required'"
+          name="category"
+          data-vv-as="カテゴリー"
+          :value="currentCategoryName"
+          :error-messages="errors.collect('category')"
+          @updateValue="$emit('selectedArticleCategory', $event)"
         >
-          ---
-        </option>
-        <option
-          v-for="(category) in categoryList"
-          :key="category.id"
-          :value="category.name"
-          :selected="currentCategoryName === category.name ? true : false "
+          <option
+            value=""
+            selected
+            disabled
+          >
+            ---
+          </option>
+          <option
+            v-for="(category) in categoryList"
+            :key="category.id"
+            :value="category.name"
+            :selected="currentCategoryName === category.name ? true : false "
+          >
+            {{ category.name }}
+          </option>
+        </app-select>
+        <app-heading :level="2">
+          タイトル・本文
+        </app-heading>
+        <div class="article-post-form">
+          <app-input
+            v-validate="'required'"
+            name="title"
+            type="text"
+            placeholder="記事のタイトルを入力してください。"
+            white-bg
+            data-vv-as="記事のタイトル"
+            :error-messages="errors.collect('title')"
+            :value="articleTitle"
+            @updateValue="$emit('editedTitle', $event)"
+          />
+        </div>
+        <div class="article-post-form">
+          <app-textarea
+            v-validate="'required'"
+            name="content"
+            placeholder="記事の本文をマークダウン記法で入力してください。"
+            white-bg
+            data-vv-as="記事の本文"
+            :error-messages="errors.collect('content')"
+            :value="articleContent"
+            @updateValue="$emit('editedContent', $event)"
+          />
+        </div>
+        <app-button
+          class="article-post-submit"
+          button-type="submit"
+          round
+          :disabled="!disabled"
+          @click="handleSubmit"
         >
-          {{ category.name }}
-        </option>
-      </app-select>
-      <app-heading :level="2">
-        タイトル・本文
-      </app-heading>
-      <app-input
-        v-validate="'required'"
-        name="title"
-        type="text"
-        placeholder="記事のタイトルを入力してください。"
-        white-bg
-        data-vv-as="記事のタイトル"
-        :error-messages="errors.collect('title')"
-        :value="articleTitle"
-        @updateValue="$emit('editedTitle', $event)"
-      />
-      <app-textarea
-        v-validate="'required'"
-        name="content"
-        placeholder="記事の本文をマークダウン記法で入力してください。"
-        white-bg
-        data-vv-as="記事の本文"
-        :error-messages="errors.collect('content')"
-        :value="articleContent"
-        @updateValue="$emit('editedContent', $event)"
-      />
-      <app-button
-        class="article-post-submit"
-        button-type="submit"
-        round
-        :disabled="!disabled"
-        @click="handleSubmit"
-      >
-        {{ buttonText }}
-      </app-button>
-    </section>
+          {{ buttonText }}
+        </app-button>
+      </section>
 
-    <article class="article-post-preview">
-      <app-markdown-preview
-        :markdown-content="markdownContent"
-      />
-    </article>
+      <article class="article-post-preview">
+        <app-markdown-preview
+          :markdown-content="markdownContent"
+        />
+      </article>
+    </div>
   </div>
 </template>
 
