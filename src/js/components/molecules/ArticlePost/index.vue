cace00a65871e19b42896cac78df699fc0dae26a
mihonakagawa <ohim212welldone@gmail.com>
2021-06-04 12:53:41 +0900

    [add]カテゴリー選択、タイトル入力、本文入力の各タグ内に@updateValue="$emit(親コンポーネントで指定してあるメソッド名)"を追加

--- src/js/components/molecules/ArticlePost/index.vue
+++ src/js/components/molecules/ArticlePost/index.vue
@@ -1,40 +1,79 @@
 <template lang="html">
-  <div>
-    <app-heading :level="1">
-      記事の新規作成
-    </app-heading>
-    <app-heading :level="2">
-      カテゴリーの選択
-    </app-heading>
-    <app-select
-      v-validate="'required'"
-      name="category"
-      data-vv-as="カテゴリー"
-      :value="currentCategoryName"
-      :error-messages="errors.collect('category')"
-    >
-      <option
-        value=""
-        selected
-        disabled
+  <div class="article-post">
+    <section class="article-post-editor">
+      <app-heading :level="1">
+        記事の新規作成
+      </app-heading>
+      <app-heading :level="2">
+        カテゴリーの選択
+      </app-heading>
+      <app-select
+        v-validate="'required'"
+        name="category"
+        data-vv-as="カテゴリー"
+        :value="currentCategoryName"
+        :error-messages="errors.collect('category')"
+        @updateValue="$emit('selectedArticleCategory', $event)"
       >
-        ---
-      </option>
-      <option
-        v-for="(category) in categoryList"
-        :key="category.id"
-        :value="category.name"
-        :selected="currentCategoryName === category.name ? true : false "
-      >
-        {{ category.name }}
-      </option>
-    </app-select>
+        <option
+          value=""
+          selected
+          disabled
+        >
+          ---
+        </option>
+        <option
+          v-for="(category) in categoryList"
+          :key="category.id"
+          :value="category.name"
+          :selected="currentCategoryName === category.name ? true : false "
+        >
+          {{ category.name }}
+        </option>
+      </app-select>
+      <app-heading :level="2">
+        タイトル・本文
+      </app-heading>
+      <app-input
+        v-validate="'required'"
+        name="title"
+        type="text"
+        placeholder="記事のタイトルを入力してください。"
+        white-bg
+        data-vv-as="記事のタイトル"
+        :error-messages="errors.collect('title')"
+        :value="articleTitle"
+        @updateValue="$emit('editedTitle', $event)"
+      />
+      <app-textarea
+        v-validate="'required'"
+        name="content"
+        placeholder="記事の本文をマークダウン記法で入力してください。"
+        white-bg
+        data-vv-as="記事の本文"
+        :error-messages="errors.collect('content')"
+        :value="articleContent"
+        @updateValue="$emit('editedContent', $event)"
+      />
+    </section>
+
+    <article class="article-post-preview">
+      <app-markdown-preview
+        :markdown-content="markdownContent"
+      />
+    </article>
   </div>
 </template>
 
 <script>
 import {
-  Heading, Input, Textarea, MarkdownPreview, Button, Select, Text,
+  Heading,
+  Input,
+  Textarea,
+  MarkdownPreview,
+  // Button,
+  Select,
+  // Text,
 } from '@Components/atoms';
 
 export default ({
