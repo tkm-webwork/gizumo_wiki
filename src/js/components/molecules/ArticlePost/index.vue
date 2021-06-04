19bc1beab9043332015da722993fd2c830e44343
mihonakagawa <ohim212welldone@gmail.com>
2021-06-04 10:09:18 +0900

    [add] <template>内にインポートしたatomsのコンポーネントを記述（heading, select）

--- src/js/components/molecules/ArticlePost/index.vue
+++ src/js/components/molecules/ArticlePost/index.vue
@@ -1,3 +1,37 @@
+<template lang="html">
+  <div>
+    <app-heading :level="1">
+      記事の新規作成
+    </app-heading>
+    <app-heading :level="2">
+      カテゴリーの選択
+    </app-heading>
+    <app-select
+      v-validate="'required'"
+      name="category"
+      data-vv-as="カテゴリー"
+      :value="currentCategoryName"
+      :error-messages="errors.collect('category')"
+    >
+      <option
+        value=""
+        selected
+        disabled
+      >
+        ---
+      </option>
+      <option
+        v-for="(category) in categoryList"
+        :key="category.id"
+        :value="category.name"
+        :selected="currentCategoryName === category.name ? true : false "
+      >
+        {{ category.name }}
+      </option>
+    </app-select>
+  </div>
+</template>
+
 <script>
 import {
   Heading, Input, Textarea, MarkdownPreview, Button, Select, Text,
