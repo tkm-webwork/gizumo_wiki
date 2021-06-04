621c283d6bf2cdf11812975b2a1ea040e9056633
mihonakagawa <ohim212welldone@gmail.com>
2021-06-04 17:04:08 +0900

    [add] errorMessageの表示のための記述を追加（v-bind, computed）

--- src/js/pages/Articles/Post.vue
+++ src/js/pages/Articles/Post.vue
@@ -4,6 +4,7 @@
     :category-list="categoryList"
     :markdown-content="markdownContent"
     :loading="loading"
+    :error-message="errorMessage"
     :access="access"
     @selectedArticleCategory="selectedArticleCategory"
     @editedTitle="editedTitle"
@@ -52,6 +53,9 @@ export default ({
     },
     loading() {
       return this.$store.state.articles.loading;
+    },
+    errorMessage() {
+      return this.$store.state.articles.errorMessage;
     },
     access() {
       return this.$store.getters['auth/access'];
