af63eb08f9da39aafd0983e2d4bde3cd73aaa3d7
坂井柾駿 <>
2021-07-26 09:47:51 +0900

    GIZUMO_wiki　タスク２　カテゴリー更新機能の追加 変更した点 ・category-editコンポーネントへプロップスでデータを渡す記述を５行目でしている ・category-editコンポーネントからemitで発火するイベントを６行目で記述している ・storeからstateを取り出している。 ・methodsに処理を追加 　---updateValue　ストア内のupdateValueを実行する処理　stateを書き換え、カテゴリー名を変更 　---updateCategory　ストア内のupdateCategoryを実行する処理　stateを新たなカテゴリーで更新する処理

--- src/js/pages/Categories/Edit.vue
+++ src/js/pages/Categories/Edit.vue
@@ -3,7 +3,12 @@
     <app-category-edit
       :disabled="loading ? true : false"
       :access="access"
+      :error-message="errorMessage"
+      :done-message="doneMessage"
+      :update-category-name="updateCategoryName"
       @clearMessage="clearMessage"
+      @handleSubmit="updateCategory"
+      @updateValue="updateValue"
     />
   </div>
 </template>
@@ -15,6 +20,8 @@ export default {
   components: {
     appCategoryEdit: CategoryEdit,
   },
+  // createdでVueインスタンスが読み込まれた時にしたい記述を書く。
+  // mountedだとエレメントが出来てからになり、画面が書き出された後になるため
   computed: {
     access() {
       return this.$store.getters['auth/access'];
@@ -22,10 +29,31 @@ export default {
     loading() {
       return this.$store.state.categories.loading;
     },
+    errorMessage() {
+      return this.$store.state.categories.errorMessage;
+    },
+    doneMessage() {
+      return this.$store.state.categories.doneMessage;
+    },
+    updateCategoryName() {
+      return this.$store.state.categories.updateCategoryName;
+    },
+  },
+  created() {
+    const { id } = this.$route.params;
+    this.$store.dispatch('categories/getCategory', id);
+    this.$store.dispatch('caterories/clearMessage');
   },
   methods: {
     clearMessage() {
       this.$store.dispatch('categories/clearMessage');
+    },
+    updateValue($event) {
+      this.$store.dispatch('categories/updateValue', $event.target.value);
+    },
+    updateCategory() {
+      if (this.loading) return;
+      this.$store.dispatch('categories/updateCategory');
     },
   },
 };
