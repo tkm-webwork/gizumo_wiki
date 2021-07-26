957b21b76c013478346e9ae95fdf974e08ecdf0d
坂井柾駿 <>
2021-07-26 09:21:52 +0900

    GIZUMO_wiki タスク２　カテゴリー更新機能追加 変更した点 ・inputエリア内で実行されるupdateValueイベントの追加 ・app-inputに渡されるプロップスを追加 ・エラーメッセージと完了メッセージをテキストではなくstoreのステートを参照し、動的に内容が書き換わるような記述 ・更新ボタンが押された際に親コンポーネントで発火するhundleSubmitイベントをemitを使用して追加

--- src/js/components/molecules/CategoryEdit/index.vue
+++ src/js/components/molecules/CategoryEdit/index.vue
@@ -9,14 +9,19 @@
       hover-opacity
       to="/categories"
     >
+    <!-- RouterLinkはaタグになる -->
       カテゴリー一覧へ戻る
     </app-router-link>
     <app-input
+      v-validate="'required'"
       class="category-management-edit__input"
       name="updateCategory"
       type="text"
       placeholder="カテゴリー名を入力してください"
-      data-vv-as=""
+      data-vv-as="カテゴリー名"
+      :error-messages="errors.collect('updateCategory')"
+      :value="updateCategoryName"
+      @updateValue="$emit('updateValue', $event)"
     />
     <app-button
       class="category-management-edit__submit"
@@ -27,12 +32,12 @@
       {{ buttonText }}
     </app-button>
 
-    <div class="category-management-edit__notice">
-      <app-text bg-error>ここにエラー時のメッセージが入ります</app-text>
+    <div v-if="errorMessage" class="category-management-edit__notice">
+      <app-text bg-error>{{ errorMessage }}</app-text>
     </div>
 
-    <div class="category-management-edit__notice">
-      <app-text bg-success>ここに更新成功時のメッセージが入ります</app-text>
+    <div v-if="doneMessage" class="category-management-edit__notice">
+      <app-text bg-success>{{ doneMessage }}</app-text>
     </div>
   </form>
 </template>
@@ -58,6 +63,18 @@ export default {
       type: Object,
       default: () => ({}),
     },
+    errorMessage: {
+      type: String,
+      default: '',
+    },
+    doneMessage: {
+      type: String,
+      default: '',
+    },
+    updateCategoryName: {
+      type: String,
+      default: '',
+    },
   },
   computed: {
     buttonText() {
@@ -70,7 +87,7 @@ export default {
       if (!this.access.edit) return;
       this.$emit('clearMessage');
       this.$validator.validate().then((valid) => {
-        if (valid) this.$emit('エミットするイベント名が入ります');
+        if (valid) this.$emit('handleSubmit');
       });
     },
   },
