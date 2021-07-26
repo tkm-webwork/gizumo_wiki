e76e27bb6e2f85740a960ae5107964cfd83d4d41
坂井柾駿 <>
2021-07-26 09:15:44 +0900

    GIZUMO_wiki　タスク２　カテゴリー更新機能の追加 変更した点 ・actionに３つの処理を追加しました。 　---getCategory　押されたカテゴリーの内容を取得して表示するもの 　---updateValue　Inputエリア内に入力されたカテゴリー名でstateを書き換える処理 　---updateCategory　更新ボタンを押された際に値を書き換えるもの
    ・それに加え、mutationsも追加しました。
    　---doneGetCategoryDetail
    　---doneUpdate
    　---updateValue

--- src/js/_store/modules/categories.js
+++ src/js/_store/modules/categories.js
@@ -71,11 +71,54 @@ export default {
         });
       });
     },
+    getCategory({ commit, rootGetters }, categoryId) {
+      axios(rootGetters['auth/token'])({
+        method: 'GET',
+        url: `/category/${categoryId}`,
+      }).then((response) => {
+        const category = response.data.category;
+        commit('doneGetCategoryDetail', category);
+      }).catch((err) => {
+        commit('failFetchCategory', { message: err.message });
+      });
+    },
+    updateValue({ commit }, categoryName) {
+      commit('updateValue', { categoryName });
+    },
+    updateCategory({ commit, rootGetters }) {
+      commit('toggleLoading');
+      const data = new URLSearchParams();
+      data.append('name', this.state.categories.updateCategoryName);
+      data.append('id', this.state.categories.updateCategoryId);
+      axios(rootGetters['auth/token'])({
+        method: 'PUT',
+        url: `/category/${this.state.categories.updateCategoryId}`,
+        data,
+      }).then((response) => {
+        const newCategory = response.data.category
+        commit('toggleLoading');
+        commit('doneUpdate', newCategory);
+      }).catch((err) => {
+        commit('failFetchCategory', { message: err.message });
+      });
+    },
   },
   mutations: {
     clearMessage(state) {
       state.errorMessage = '';
       state.doneMessage = '';
+    },
+    doneGetCategoryDetail(state, category) {
+      state.updateCategoryName = category.name;
+      state.updateCategoryId = category.id;
+    },
+    doneUpdate(state, newCategory) {
+      state.updateCategoryId = newCategory.id;
+      state.updateCategoryName = newCategory.name;
+      state.doneMessage = '更新が成功しました。';
+    },
+    updateValue(state, { categoryName }) {
+      state.updateCategoryName = categoryName;
     },
     doneGetAllCategories(state, { categories }) {
       state.categoryList = [...categories];
