21d3d86245798c6e35c09adf176149074c7a8bf4
鈴木孝太郎 <k.chanmpmp@gmail.com>
2021-07-29 12:51:21 +0900

    add: CategoryEditのインプットタグのバリデート追加の記述

--- src/js/components/molecules/CategoryEdit/index.vue
+++ src/js/components/molecules/CategoryEdit/index.vue
@@ -12,11 +12,13 @@
       カテゴリー一覧へ戻る
     </app-router-link>
     <app-input
+      v-validate="'required'"
       class="category-management-edit__input"
       name="updateCategory"
       type="text"
       placeholder="カテゴリー名を入力してください"
-      data-vv-as="カテゴリー"
+      data-vv-as="カテゴリー名"
+      :error-messages="errors.collect('updateCategory')"
       :value="editCategoryName"
       @updateValue="$emit('updateValue', $event)"
     />
